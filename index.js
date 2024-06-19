bb945ad452cd4134c0f18fe4ac7a7d81a3fae1f6
HaiNamm <dhtn8xx@gmail.com>
2024-06-19 11:57:07 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.use(morgan('combined'))
-
-app.get('/tin-tuc', (req, res) => {
+app.get('/trang-chu', (req, res) => {
   res.send('Hello World!')
 })
 
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
