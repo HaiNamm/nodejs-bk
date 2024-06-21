3cec993aded1958fe7b8e58f115a4ef3536b9ae1
HaiNamm <dhtn8xx@gmail.com>
2024-06-21 14:54:09 +0700

    fdasf

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-      extended: true,
-}));
-    app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //HTTP logger
 // app.use(morgan('combined'));
@@ -23,28 +25,30 @@ app.engine(
     }),
 );
 
-      app.set('view engine', 'hbs');
-      app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-    app.get('/', (req, res) => {
-        res.render('home');
-    });
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
-      app.get('/news',
-        
-        (req, res) => {
-          res.render('news');
-      });
+app.get(
+    '/news',
 
-        app.get('/search', (req, res) => {
-            res.render('search');
-        });
+    (req, res) => {
+        res.render('news');
+    },
+);
 
-  app.post('/search', (req, res) => {
-      console.log(req.body);
-      res.send('');
-  });
+app.get('/search', (req, res) => {
+    res.render('search');
+});
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
