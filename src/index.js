3b225ef52fdc685875d6855257f7c03090d7deb9
HaiNamm <dhtn8xx@gmail.com>
2024-06-21 15:08:04 +0700

    fdasfa

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+   express.urlencoded({
+      extended: true,
+   }),
 );
 app.use(express.json());
 
@@ -19,32 +19,32 @@ app.use(express.json());
 
 //Template engine
 app.engine(
-  'hbs',
-  engine({
-    extname: '.hbs',
-  }),
+   'hbs',
+   engine({
+      extname: '.hbs',
+   }),
 );
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+   res.render('home');
 });
 
 app.get('/news', (req, res) => {
-  res.render('news');
+   res.render('news');
 });
 
 app.get('/search', (req, res) => {
-  res.render('search');
+   res.render('search');
 });
 
 app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('');
+   console.log(req.body);
+   res.send('');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+   console.log(`Example app listening on port ${port}`);
 });
