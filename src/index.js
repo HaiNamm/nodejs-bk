34fc27508eebcd13177e0a77e23ae21721d4aaed
HaiNamm <dhtn8xx@gmail.com>
2024-06-21 13:45:23 +0700

    install prettier lint-stage husky

--- src/index.js
+++ src/index.js
@@ -8,25 +8,44 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-    extname: '.hbs'
-}));
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+);
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+            app.get('/news', (req, res) => {
+              res.render('news');
+            });
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
