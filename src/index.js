0210af815e86c249b14fce0453da83f7a90b3a8a
HaiNamm <dhtn8xx@gmail.com>
2024-06-21 16:16:23 +0700

    done

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 
-const app = express();
-const port = 3000;
+    const app = express();
+      const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
